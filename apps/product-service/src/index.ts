3ee32db3b98a08e2f5d5343b2dd4b3d4656d0960
ru70129 <ru70129@ubt-uni.net>
2026-01-09 21:11:32 +0100

    error handling

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,6 +1,11 @@
 import express from "express"
+import cors from "cors";
 
 const app = express()
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:4200"],
+    credentials: true,
+}))
 
 app.listen(8000, () =>{
     console.log("Product service is running on port 8000")
